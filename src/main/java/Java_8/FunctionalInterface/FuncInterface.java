34de8de3661031a6f9946102de98cdc86c23b19f
Jhanvi Chhatbar <jhanvi.chhatbar31@gmail.com>
2024-10-13 21:36:08 +0530

    core_java

---
+++ src/main/java/Java_8/FunctionalInterface/FuncInterface.java
@@ -0,0 +1,7 @@
+package Java_8.FunctionalInterface;
+
+@FunctionalInterface
+public interface FuncInterface {
+
+    public abstract float calculate(int a, int b, int c);
+}
---
+++ src/main/java/Java_8/FunctionalInterface/TestFuncInterface.java
@@ -0,0 +1,4 @@
+package Java_8.FunctionalInterface;
+
+public class TestFuncInterface {
+}
